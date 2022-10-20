80e9c1f5bc1e1b935ee5cab9b3bd39737d4a99bd
Marcel Balint <blank_blankness@yahoo.com>
2022-10-20 09:29:05 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./compnents/ExpenseItem";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <ExpenseItem />
+      <h1>This is the title</h1>
+      <p>dwcw</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/compnents/ExpenseItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExpenseItem = () => {
+  return <div>This is the Item</div>;
+};
+
+export default ExpenseItem;
