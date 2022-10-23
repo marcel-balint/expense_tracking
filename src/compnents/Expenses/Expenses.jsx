10cf4a957e22fd07749cefaa7de0ca288a870e73
Marcel Balint <blank_blankness@yahoo.com>
2022-10-23 07:59:23 -0400

    Implemented the filter functionality

--- src/compnents/Expenses/Expenses.jsx
+++ src/compnents/Expenses/Expenses.jsx
@@ -10,13 +10,18 @@ function Expenses(props) {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
+
   return (
     <div className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((expense) => (
+      {filteredExpenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
