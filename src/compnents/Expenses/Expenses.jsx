e23693d9c3023c3d53e80449be02f39eec8cceba
Marcel Balint <blank_blankness@yahoo.com>
2022-10-23 09:33:28 -0400

    Added a chart which indicates the expenses for every month

--- src/compnents/Expenses/Expenses.jsx
+++ src/compnents/Expenses/Expenses.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2022");
@@ -21,6 +22,7 @@ function Expenses(props) {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </div>
   );
