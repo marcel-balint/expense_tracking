c4cc699443edac73a490e13c3772493a4547c5cb
Marcel Balint <blank_blankness@yahoo.com>
2022-10-23 05:34:14 -0400

    Added two-way data binding

--- src/compnents/NewExpense/ExpenseForm.jsx
+++ src/compnents/NewExpense/ExpenseForm.jsx
@@ -26,13 +26,20 @@ const ExpenseForm = () => {
       date: new Date(enteredDate),
     };
     console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amont</label>
@@ -40,6 +47,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -49,6 +57,7 @@ const ExpenseForm = () => {
             type="date"
             min="2015-01-01"
             max="2023-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
