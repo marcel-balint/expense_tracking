afce4b77383a97888d6220da8297c4f4baed968b
Marcel Balint <blank_blankness@yahoo.com>
2022-10-23 06:23:50 -0400

    Created the filter compenent

--- src/compnents/Expenses/Expenses.jsx
+++ src/compnents/Expenses/Expenses.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2022");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <div className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
